a4ba3c1fe5993e7c78ebfc5bb72a9e2679d0f04d
Yuda Media <yudamedia@gmail.com>
2025-06-21 15:35:03 +0000

    Add Daraja integration setup button in TukTuk Settings
    - Implemented a custom button in the TukTuk Settings form to initiate Daraja integration setup.
    - Added a function to handle the integration process and display a confirmation message upon completion.

--- tuktuk_management/tuktuk_management/doctype/tuktuk_settings/tuktuk_settings.js
+++ tuktuk_management/tuktuk_management/doctype/tuktuk_settings/tuktuk_settings.js
@@ -1,8 +1,22 @@
 // Copyright (c) 2024, Yuda Media and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("TukTuk Settings", {
-// 	refresh(frm) {
+// In tuktuk_settings.js
+frappe.ui.form.on("TukTuk Settings", {
+    refresh: function(frm) {
+        frm.add_custom_button(__('Setup Daraja Integration'), function() {
+            setup_daraja_integration();
+        });
+    }
+});
 
-// 	},
-// });
+function setup_daraja_integration() {
+    frappe.call({
+        method: 'tuktuk_management.api.tuktuk.setup_daraja_integration',
+        callback: function(r) {
+            if (r.message) {
+                frappe.msgprint('Daraja integration setup completed');
+            }
+        }
+    });
+}
